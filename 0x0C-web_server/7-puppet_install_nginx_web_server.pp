9465484b570bb4bdced9525a342cd33f404c3098
Jesulayomy <jesulayomi081@gmail.com>
2023-04-24 18:20:23 +0000

    [Fixes] Web server files

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,6 @@
-# Updates, installs nginx server, says hello, give scustom redirect, gives custom 404
+# Installs a Nginx server, sets hello to file, allows redirection
 
-exec { 'install':
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;',
-  before   => Exec['files']
-}
-
-exec { 'files':
-  provider => shell,
-  command  => 'sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html ; sudo ufw allow "Nginx HTTP"',
-  before   => Exec['sed']
-}
-
-exec { 'sed':
-  provider => shell,
-  command  => 'sed -i "s/server_name  _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;\n\n\terror_page 404 \/custom_404_not_found.html;\n\tlocation = \/custom_404_not_found.html {\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-available/default',
-  before   => Exec['initialize']
-}
-
-exec { 'initialize':
-  provider => shell,
-  command  => 'sudo service nginx start'
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
