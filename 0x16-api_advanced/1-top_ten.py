49e55837e14e78a7f0838c108530c6a2c29af77a
Jesulayomy <jesulayomi081@gmail.com>
2023-06-07 14:22:57 +0000

    Fix: Pep8, lower case matching

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Function to print the top ten posts 
+    Function to print the top ten posts
     on a given Reddit subreddit.
 """
 import requests
@@ -15,8 +15,11 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    res = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    res = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
     if res.status_code == 404:
         print("None")
         return
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -42,9 +42,9 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
             if word.lower() in title:
                 times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
-                    instances[word] = times
+                    instances[word.lower()] = times
                 else:
-                    instances[word] += times
+                    instances[word.lower()] += times
 
     if after is None:
         if len(instances) == 0:
